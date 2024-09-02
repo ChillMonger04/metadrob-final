f6cabb16a1e2d1ee91023c9f44d47c5766691ef7
Jasjeet <jasjeet_mann@yahoo.com>
2024-09-02 20:55:26 +0530

    changed the cursor

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,3 @@
-// import { useEffect, useRef } from "react";
-// import LocomotiveScroll from "locomotive-scroll";
-// import { gsap } from "gsap";
-// import { ScrollTrigger } from "gsap/ScrollTrigger";
-// import "./locomotive-scroll.css"; // Ensure this path is correct
-
 import Introduction from "./components/Introduction";
 import Solutions from "./components/Solutions";
 import Features from "./components/Features";
@@ -15,34 +9,10 @@ import ContactForm from "./components/ContactForm";
 import CustomCursor from "./components/CustomCursor";
 
 const App = () => {
-  // const scrollRef = useRef(null);
-
-  // useEffect(() => {
-  //   // Initialize Locomotive Scroll
-  //   gsap.registerPlugin(ScrollTrigger);
-  //   if (scrollRef.current) {
-  //     const scroll = new LocomotiveScroll({
-  //       el: scrollRef.current,
-  //       smooth: true,
-  //       inertia: 0.8, // Adjust for performance
-  //       multiplier: 0.8, // Adjust for performance
-  //       smoothMobile: true, // Ensure smooth scrolling on mobile
-  //     });
-
-  //     return () => {
-  //       scroll.destroy();
-  //     };
-  //   }
-  // }, []);
-
   return (
-    <div
-      className="projectWrapper scroll-container"
-      // data-scroll-container
-      // ref={scrollRef}
-    >
+    <>
       <CustomCursor />
-      <div className="bg-black z-20">
+      <div className="componentContainer bg-black">
         <Introduction />
         <Solutions />
         <Features />
@@ -52,7 +22,7 @@ const App = () => {
         <Testimonials />
         <ContactForm />
       </div>
-    </div>
+    </>
   );
 };
 
--- src/components/CustomCursor.jsx
+++ src/components/CustomCursor.jsx
@@ -1,58 +1,70 @@
-import { useEffect, useRef, useState } from 'react';
-
+import React, { useEffect, useRef } from "react";
 
 const CustomCursor = () => {
   const cursorRef = useRef(null);
   const trailContainerRef = useRef(null);
-  const [hideCursorTimeout, setHideCursorTimeout] = useState(null);
-
-  const createTrail = (x, y) => {
-    const trail = document.createElement('div');
-    trail.classList.add('trail');
-    trail.style.left = `${x - 75}px`;
-    trail.style.top = `${y - 75}px`;
-    trail.style.opacity = 1;
-    trailContainerRef.current.appendChild(trail);
-
-    setTimeout(() => {
-      trail.style.opacity = 0;
-      setTimeout(() => trail.remove(), 1000);
-    }, 1000);
-  };
+  const trailElements = useRef([]);
+  const isMouseMoving = useRef(false);
+  const hideCursorTimeoutRef = useRef(null);
 
   useEffect(() => {
-    const handleMouseMove = (e) => {
-      clearTimeout(hideCursorTimeout);
+    const cursor = cursorRef.current;
+    const trailContainer = trailContainerRef.current;
 
-      cursorRef.current.style.left = `${e.clientX}px`;
-      cursorRef.current.style.top = `${e.clientY}px`;
-      cursorRef.current.style.opacity = 1;
+    const createTrail = (x, y) => {
+      const trail = document.createElement("div");
+      trail.classList.add("trail");
+      trail.style.left = `${x - 75}px`;
+      trail.style.top = `${y - 75}px`;
+      trail.style.opacity = 1;
+      trailContainer.appendChild(trail);
+
+      setTimeout(() => {
+        trail.style.opacity = 0;
+        setTimeout(() => trail.remove(), 1000);
+      }, 1000);
+
+      trailElements.current.push(trail);
+      if (trailElements.current.length > 20) {
+        let oldTrail = trailElements.current.shift();
+        oldTrail.remove();
+      }
+    };
+
+    const handleMouseMove = (e) => {
+      clearTimeout(hideCursorTimeoutRef.current);
+      isMouseMoving.current = true;
+
+      cursor.style.left = `${e.clientX}px`;
+      cursor.style.top = `${e.clientY}px`;
+      cursor.style.opacity = 1;
 
       createTrail(e.clientX, e.clientY);
 
-      setHideCursorTimeout(setTimeout(() => {
-        cursorRef.current.style.opacity = 0;
-      }, 500));
+      hideCursorTimeoutRef.current = setTimeout(() => {
+        cursor.style.opacity = 0;
+        isMouseMoving.current = false;
+      }, 500);
     };
 
     const handleMouseOut = () => {
-      cursorRef.current.style.opacity = 0;
+      cursor.style.opacity = 0;
     };
 
-    document.addEventListener('mousemove', handleMouseMove);
-    document.addEventListener('mouseout', handleMouseOut);
+    document.addEventListener("mousemove", handleMouseMove);
+    document.addEventListener("mouseout", handleMouseOut);
 
     return () => {
-      document.removeEventListener('mousemove', handleMouseMove);
-      document.removeEventListener('mouseout', handleMouseOut);
+      document.removeEventListener("mousemove", handleMouseMove);
+      document.removeEventListener("mouseout", handleMouseOut);
     };
-  }, [hideCursorTimeout]);
+  }, []);
 
   return (
-    <>
+    <div className="cursor-container">
       <div id="cursor" className="cursor" ref={cursorRef}></div>
       <div className="trail-container" ref={trailContainerRef}></div>
-    </>
+    </div>
   );
 };
 
